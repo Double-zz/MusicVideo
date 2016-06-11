3b380040228b2adc0e353b71589194f0f24b33a2
Double-zz <ffxdz123@gmail.com>
2016-06-11 16:10:11 +0800

    complete part-3

--- MusicVideo/APIManager.swift
+++ MusicVideo/APIManager.swift
@@ -17,27 +17,46 @@ class APIManager {
         let session = NSURLSession(configuration: config)
         
         let url = NSURL(string: urlString)!
-        
+    
         let task = session.dataTaskWithURL(url) {
             (data, response, error) -> Void in
-            dispatch_async(dispatch_get_main_queue()){
-                if error != nil {
-                    completion(result: (error!.localizedDescription))
-                } else {
-                    completion(result: "NSURLSession success")
-                    print(data)
+            
+            if error != nil {
+                
+                dispatch_async(dispatch_get_main_queue()){
+                    completion(result: error!.localizedDescription)
+                }
+                
+            } else {
+                
+                // add JSON
+                do {
+                 
+                    if let json = try NSJSONSerialization.JSONObjectWithData(data!, options: .AllowFragments) as? [String: AnyObject] {
+                        print(json)
+                        
+                        let priority = DISPATCH_QUEUE_PRIORITY_HIGH
+                        dispatch_async(dispatch_get_global_queue(priority, 0)) {
+                            dispatch_async(dispatch_get_main_queue()) {
+                                completion(result: "JSONSerialization successful")
+                            }
+                        }
+                    }
+                 //the end of do
+                } catch {
+                    dispatch_async(dispatch_get_main_queue()){
+                        completion(result: "error in JSONSerialization")
+                    }
                 }
             }
+            
         }
-        task.resume()
+        task.resume()  
+        
+
+        
+        //the end of loadData
     }
     
-    
-    
-    
-    
-    
-    
-    
-    
+    //the end of APIManager
 }
