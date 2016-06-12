df8d574673a5acba77a144cdcad5d50eee81eecd
Double-zz <ffxdz123@gmail.com>
2016-06-12 16:53:29 +0800

    add some new feature

--- MusicVideo/MusicVideo.swift
+++ MusicVideo/MusicVideo.swift
@@ -13,6 +13,15 @@ class Videos {
     private var _vName: String
     private var _vImageUrl: String
     private var _vVideoUrl: String
+    private var _vRights: String
+    private var _vPrice: String
+    private var _vArtist: String
+    private var _vImid: String
+    private var _vgenre: String
+    private var _vLinkToiTunes: String
+    private var _vReleaseDate: String
+    
+    var vImageData: NSData?
     
     var vName: String{
         return _vName
@@ -26,6 +35,37 @@ class Videos {
         return _vVideoUrl
     }
     
+    var vRights: String {
+        return _vRights
+    }
+    
+    var vPrice: String {
+        return _vPrice
+    }
+    
+    var vArtist: String {
+        return _vArtist
+    }
+    
+    var vImid: String {
+        return _vImid
+    }
+    
+    var vGenre: String {
+        return _vgenre
+    }
+    
+    var vLinkToiTunes : String {
+        return _vLinkToiTunes
+    }
+    
+    var vReleaseDate: String {
+        return _vReleaseDate
+    }
+    
+    
+    
+    
     init(data: JSONDictionary) {
         
         if let name = data["im:name"] as? JSONDictionary,
@@ -38,8 +78,7 @@ class Videos {
         
         if let image = data["im:image"] as? JSONArray,
             let videoImage = image[3] as? String {
-            
-            _vImageUrl = videoImage.stringByReplacingOccurrencesOfString("100x100", withString: "600x600")
+            self._vImageUrl = videoImage.stringByReplacingOccurrencesOfString("100x100", withString: "600x600")
         }else {
             _vImageUrl = ""
         }
@@ -51,6 +90,60 @@ class Videos {
         } else {
             _vVideoUrl = ""
         }
+        
+        if let rights = data["rights"] as? JSONDictionary,
+            let vRights = rights["label"] as? String {
+            self._vRights = vRights
+        } else {
+            _vRights = ""
+        }
+        
+        if let price = data["im:price"] as? JSONDictionary,
+            let vPrice = price["label"] as? String {
+            self._vPrice = vPrice
+        } else {
+            _vPrice = ""
+        }
+        
+        if let artist = data["im:artist"] as? JSONDictionary,
+            let imArtist = artist["label"] as? String {
+            self._vArtist = imArtist
+        } else {
+            _vArtist = ""
+        }
+        
+        if let imid = data["id"] as? JSONDictionary,
+        let attributes = imid["attributes"] as? JSONDictionary,
+            let attId = attributes["im:id"] as? String {
+            self._vImid = attId
+        } else {
+            _vImid = ""
+        }
+        
+        if let genre = data["category"] as? JSONDictionary,
+        let attributesGenre = genre["attributes"] as? JSONDictionary,
+            let videoGenre = attributesGenre["term"] as? String {
+            self._vgenre = videoGenre
+        } else {
+            _vgenre = ""
+        }
+        
+        if let toiTunes = data["id"] as? JSONDictionary,
+            let theLink = toiTunes["label"] as? String {
+            self._vLinkToiTunes = theLink
+        } else {
+            _vLinkToiTunes = ""
+        }
+        
+        if let releaseDate = data["im:releaseDate"] as? JSONDictionary,
+            let theDate = releaseDate["label"] as? String {
+            self._vReleaseDate = theDate
+        } else {
+            _vReleaseDate = ""
+        }
+        
+        
+    // end of init
     }
     
     
