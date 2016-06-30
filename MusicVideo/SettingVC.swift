ba5e059319ab5811a9d50816d9e0da348f235f48
Double-zz <ffxdz123@gmail.com>
2016-06-30 16:59:38 +0800

    add setting view

---
+++ MusicVideo/SettingVC.swift
@@ -0,0 +1,13 @@
+//
+//  SettingVC.swift
+//  MusicVideo
+//
+//  Created by Veronica on 16/6/30.
+//  Copyright © 2016年 Veronica. All rights reserved.
+//
+
+import UIKit
+
+class SettingVC: UITableViewController {
+
+}
